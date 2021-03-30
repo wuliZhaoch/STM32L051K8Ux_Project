e2bd7329469028fd0cb6ca276cb6bcd6d9374210
wuliZhaoch <Zhao_ch@163.com>
2021-03-30 10:17:26 +0800

    Add Led status function and low power config function

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,6 +2,28 @@
 
 uint32_t main_loop = 0;
 
+void LED_Blink(uint16_t Blink_counter, uint16_t period)
+{
+    for (uint16_t i = 0; i < Blink_counter; i++)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
+        HAL_Delay(period);
+    }
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+}
+
+void Enter_into_lowpower_config(void)
+{
+    /* Enable Ultra low power mode */
+    HAL_PWREx_EnableUltraLowPower();
+    /* Enable the fast wake up from Ultra low power mode */
+    HAL_PWREx_EnableFastWakeUp();
+    /* Select HSI as system clock source after Wake Up from Stop mode */
+    __HAL_RCC_WAKEUPSTOP_CLK_CONFIG(RCC_STOP_WAKEUPCLOCK_HSI);
+}
+
+
 /**
  * @brief  The application entry point.
  * @retval int
@@ -20,10 +42,11 @@ int main(void)
 
     while (1)
     {
-        printf("System mainloop is : %ld\r\n", main_loop);
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
-        main_loop++;
-        HAL_Delay_US(1000000);
+        LED_Blink(10, 200);
+
+//        printf("System mainloop is : %ld\r\n", main_loop);
+//        main_loop++;
+//        HAL_Delay_US(1000000);
 
     }
 
