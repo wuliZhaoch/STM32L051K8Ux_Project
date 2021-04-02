2cc5efe0346556eab7389e701d833d4e203300b6
Zhao_ch <Zhao_ch@163.com>
2021-04-02 10:01:49 +0800

    Create chip_id.h and chip_id.c

---
+++ Core/Inc/chip_id.h
@@ -0,0 +1,19 @@
+/*
+ * chip_id.h
+ *
+ *  Created on: 2021-04-02
+ *      Author: wuliZhao_ch
+ */
+#ifndef CHIP_ID_H_
+#define CHIP_ID_H_
+
+#include "main.h"
+#include "system.h"
+
+
+
+
+
+
+
+#endif
---
+++ Core/Src/chip_id.c
@@ -0,0 +1,8 @@
+/*
+ * chip_id.c
+ *
+ *  Created on: 2020-04-02
+ *      Author: wuliZhao_ch
+ *       brief: Read Chip Unique ID (96-bit)
+ */
+#include "chip_id.h"
